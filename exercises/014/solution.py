4c11525b5df52751091f8c0c85bacdf54dbb5b0f
Clémence <clemence.labarre@cri-paris.com>
2016-12-21 15:58:50 +0100

    up

--- exercises/014/solution.py
+++ exercises/014/solution.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
-i = len(sys.argv[1])
+i = len(sys.argv)
 if i == 1:
    print('usage: python3 solution.py PARAM')
 elif i != 1:
-   print(sys.argv[1])
+   print(sys.argv[0])
