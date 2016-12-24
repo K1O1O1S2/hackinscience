c9737d1a19df62f7e7fb7b4ee1677aca1d4f2da4
K1O1O1S2 <clemence.labarre@cri-paris.org>
2016-12-24 22:06:37 +0100

    Print every two letters pairs, without duplicates

---
+++ exercises/070/solution.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+alphabet = range(97, 123, 1)
+i = 0
+for letter1 in alphabet:
+    for letter2 in alphabet:
+        if letter1 == letter2:
+            print(chr(letter1) + chr(letter2))
