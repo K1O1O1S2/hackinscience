7987fa88b2b8015501b983fa3a0a984ccd37a175
K1O1O1S2 <clemence.labarre@cri-paris.org>
2016-12-24 22:09:18 +0100

    Update solution.py

--- exercises/070/solution.py
+++ exercises/070/solution.py
@@ -3,5 +3,5 @@ alphabet = range(97, 123, 1)
 i = 0
 for letter1 in alphabet:
     for letter2 in alphabet:
-        if letter1 == letter2:
+        if letter1 != letter2:
             print(chr(letter1) + chr(letter2))
