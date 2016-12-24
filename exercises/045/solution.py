bd48718cd71d95e7518f55d23664dea6ab8dbcc7
Clémence <clemence.labarre@cri-paris.com>
2016-12-24 21:24:52 +0100

    return instead of print

--- exercises/045/solution.py
+++ exercises/045/solution.py
@@ -4,8 +4,8 @@ import math
 
 def sqrt(num):
     if num == 0:
-        print(0)
+        return 0
     elif num < 0:
-        print(abs(num) ** 0.5)
+        return -num ** 0.5
     else:
-        print(num ** 0.5)
+        return num ** 0.5
