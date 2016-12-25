aa42c0fe2009373f60f7d1503d4c23f6deb4bfc6
K1O1O1S2 <clemence.labarre@cri-paris.org>
2016-12-25 14:41:24 +0100

    sort_students

---
+++ exercises/085/solution.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import operator
+def sort_a_list(l):
+    getcount = itemgetter(1)
+    sorted(l, key=getcount)
+
+
+def sort_by_mark(l):
+    getcount = itemgetter(1)
+    map(getcount, l)
+    sorted(l, key=getcount)
+
+
+def sort_by_name(l):
+    itemgetter(item)
