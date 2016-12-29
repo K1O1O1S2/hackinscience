163f6bc49f8bc340a11d57dda011009a98e3930d
Clémence <clemence.labarre@cri-paris.com>
2016-12-29 15:05:19 +0100

    solution

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -2,4 +2,9 @@
 import time
 import datetime
 n = datetime.datetime.now()
-print("Today is %s-%s-%s and it is %s:%s:%s") % (n.year, n.month, n.day, n.hour, n.minute, n.second)
+d = datetime.date.today()
+h = n.hour , n.minute , n.second
+print("Today is")
+print(d)
+print("and it is")
+print(h)
