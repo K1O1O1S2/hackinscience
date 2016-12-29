24205cf96f5412c332792af2e44b715f784341b7
Clémence <clemence.labarre@cri-paris.com>
2016-12-29 14:45:53 +0100

    solution

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -2,4 +2,4 @@
 import time
 import datetime
 n = datetime.datetime.now()
-print("Today is"), ; print(datetime.date.today()), ; print('and it is'), ; print(n.hour),;print(":"),;print(n.minute),; print":",; print(n.second)
+print("Today is"), ; print(datetime.date.today()), ; print('and it is'), ; print(n.hour),;print(":"),; print(n.minute),; print":",; print(n.second)
