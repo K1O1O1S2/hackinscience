65585b710fa2235427328a643492e894f26966ab
Clémence <clemence.labarre@cri-paris.com>
2016-12-29 14:56:03 +0100

    solution

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -2,4 +2,4 @@
 import time
 import datetime
 n = datetime.datetime.now()
-print("Today is %s-%s-%s and it is %s:%s:%S") % (n.year, n.month, n.day, n.hour, n.minute, n.second)
+print("Today is %s-%s-%s and it is %s:%s:%s") % (n.year, n.month, n.day, n.hour, n.minute, n.second)
