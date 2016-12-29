81dfd8048dc17b7c6760a8c66e51ce86fb993b40
Clémence <clemence.labarre@cri-paris.com>
2016-12-29 14:44:17 +0100

    solution

---
+++ exercises/025/solution.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+import time
+import datetime
+n = datetime.datetime.now()
+print("Today is"), ; print(datetime.date.today()), ; print('and it is'), ; print(n.hour),;print(":"),;print(n.minute),; print":",; print(n.second)
